1140955ff26a33bf7560f2799530675225ed01a0
louismerlin <louis.merlin@epfl.ch>
2016-11-10 12:26:15 +0100

    Comment script

--- secretsanta.rb
+++ secretsanta.rb
@@ -1,14 +1,15 @@
+# Dependencies
 require 'gmail'
 require 'io/console'
 
-
-
+# Import people from people.txt
 people = []
 File.open("people.txt").each_line do |line|
     data = line.split(" ")
     people.push({name: data[0], email: data[1]})
 end
 
+# Create the present repartition
 begin
     list = []
     people.each_index {|i| list.push(i)}
@@ -17,12 +18,16 @@ begin
         person[:chosen] = list.delete_at(ran)
     end
 end while people.any? {|person| people.index(person)==person[:chosen]}
+# Restart if someone has himself assigned
 
+# Gmail login
 puts "Gmail username: "
 username = gets.chomp
 puts "Password: "
 password = STDIN.noecho(&:gets).chomp
 gmail = Gmail.connect(username, password)
+
+# Send mails
 for person in people
     gmail.deliver do
         to person[:email]
@@ -30,4 +35,5 @@ for person in people
         body "Hi " + person[:name] + "!\n\nYour secret santa match this year is " + people[person[:chosen]][:name] + ".\n\nWith love,\nSanta"
     end
 end
+
 gmail.logout
